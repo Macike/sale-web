0926d1f98fae56f33e5baf58e25f34cc7d96b28b
maca <marcela.zubajova@gmail.com>
2018-03-25 17:21:12 +0200

    function which does its functionality

---
+++ script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+/**
+ * Funcion does its funcionality which is populating everything.
+ *
+ * @param {Object} e event object
+ */
+let addElement = (e)=>
+{
+    /*let input = document.createElement("input");
+    input.type = "text";
+    let main = document.getElementsByTagName("main");
+    main[0].appendChild(input);*/
+
+    let arr = [
+                "images/boty1.jpg",
+                "images/boty2.jpg",
+                "images/boty3.jpg",
+                "images/boty4.jpg",
+                "images/boty5.jpg",
+                "images/boty6.jpg",
+                "images/boty7.jpg",
+                "images/boty8.jpg"];
+
+  let len = arr.length;
+  let rows = parseInt(len /4, 10);
+  let index = 0;
+  let init = 0;
+  let divs = [];
+  let main = document.getElementsByTagName("main")[0];
+  for (let i = 0; i < rows; i++)
+  {
+    divs[i]= document.createElement("div");
+    divs[i].className = "row text-center imigi";
+    index += 4;
+    let cols = [];
+    let images = [];
+    for (let j = init; j < index; j++)
+    {
+      let ind = j % 4;
+      images[ind] = document.createElement("img");
+      images[ind].className = "special";
+      images[ind].src = arr[j];
+
+      cols[ind]= document.createElement("div");
+      cols[ind].className = "column col-md-3";
+
+      cols[ind].appendChild(images[ind]);
+      divs[i].appendChild(cols[ind]);
+    }
+
+    init += 4;
+    main.appendChild(divs[i]);
+  }
+
+
+
+}
+
+window.addEventListener("load", addElement, "false");
