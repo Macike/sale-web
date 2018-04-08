bd4187c855ea249c1d5de1190e77824177ae7566
maca <marcela.zubajova@gmail.com>
2018-04-08 19:59:42 +0200

    layout via js

--- script.js
+++ script.js
@@ -7,42 +7,130 @@
  */
 let addElement = (e)=>
 {
-    /*let input = document.createElement("input");
-    input.type = "text";
-    let main = document.getElementsByTagName("main");
-    main[0].appendChild(input);*/
-
     let arr = [
-                "images/boty1.jpg",
+                ["images/boty1.jpg",
                 "images/boty2.jpg",
                 "images/boty3.jpg",
                 "images/boty4.jpg",
                 "images/boty5.jpg",
                 "images/boty6.jpg",
                 "images/boty7.jpg",
-                "images/boty8.jpg"];
+                "images/boty8.jpg"],
+
+                ["images/topanky1.jpg",
+                "images/topanky2.jpg",
+                "images/topanky3.jpg",
+                "images/topanky4.jpg",
+                "images/topanky5.jpg",
+                "images/topanky6.jpg",
+                "images/topanky7.jpg",
+                "images/topanky8.jpg"],
+
+                ["images/boticky1.jpg",
+                "images/boticky2.jpg",
+                "images/boticky3.jpg",
+                "images/boticky4.jpg",
+                "images/boticky5.jpg",
+                "images/boticky6.jpg",
+                "images/boticky7.jpg",
+                "images/boticky8.jpg"]
+              ];
 
     let labels =[
-                  ["Originals", "99,99€"],
+
+                  [["Originals", "99,99€"],
                   ["Tubular Shadows", " 69,96€"],
                   ["Gazelle Super", " 76,96€"],
-                  [" Campus", "99,99€"],
+                  ["Campus", "99,99€"],
                   ["Superstar", "79,95€"],
                   ["Pureboost X TR ", "139,95€"],
                   ["CrazyTrain Pro", "69,99€"],
-                  ["Ultra Boost ", "125,96€"]
-                ]
+                  ["Ultra Boost ", "125,96€"]],
 
+                  [["Originals", "99,99€"],
+                  ["Tubular Shadows", " 69,96€"],
+                  ["Gazelle Super", " 76,96€"],
+                  ["Campus", "99,99€"],
+                  ["Superstar", "79,95€"],
+                  ["Pureboost X TR ", "139,95€"],
+                  ["Train Pro", "69,99€"],
+                  ["Ultra Speed ", "125,96€"]],
 
-  /*OTRAVA*/
-  let x=5;
-  let y=4;
-  let z=x;
-  x=y;
-  y=z;
+                  [["Originals", "99,99€"],
+                  ["Tubular Shadows", " 69,96€"],
+                  ["Gazelle Super", " 76,96€"],
+                  ["Originals", "99,99€"],
+                  ["Superstar", "79,95€"],
+                  ["Pureboost X TR ", "139,95€"],
+                  ["CrazyTrain Pro", "69,99€"],
+                  ["Ultra Boost ", "125,96€"]]
+                ];
 
+  let names=[
+              "Dámske",
+              "Pánske",
+              "Detské"
+            ];
 
-  let len = arr.length;
+   let len = names.length;
+
+   let main = document.getElementsByTagName("main")[0];
+   for(let i = 0; i < len ; i++)
+   {
+
+     let div = document.createElement("div");
+     div.className ="row text-center titles";
+
+     let child_div = document.createElement("div");
+     child_div.className = "col-12";
+
+     let h1 = document.createElement("h1");
+     h1.className = "title";
+     h1.innerText = names[i];
+
+     child_div.appendChild(h1);
+     div.appendChild(child_div);
+     main.appendChild(div);
+
+     let row_len = arr[i].length;
+     let rows = Math.ceil(row_len / 4);
+     let start = 0;
+     let end = 0;
+     for (let j = 0; j < rows; j++)
+     {
+       let row = document.createElement("div");
+       row.className = "row text-center imigi";
+       end += 4;
+
+       for (let k = start; k < end && k < row_len; k++)
+       {
+           let col = document.createElement("div");
+           col.className ="column col-md-3";
+           let a = document.createElement("a");
+           a.className="sortiment";
+           let img = document.createElement("img");
+           img.className="special";
+           img.src = arr[i][k];
+           img.alt = "Sale product" + k;
+           a.appendChild(img);
+           let name = document.createElement("p");
+           name.innerText=labels[i][k][0];
+           let price = document.createElement("p");
+           price.innerText=labels[i][k][1];
+
+           col.appendChild(a);
+           col.appendChild(name);
+           col.appendChild(price);
+           row.appendChild(col);
+       }
+
+       main.appendChild(row);
+       start += 4;
+     }
+
+   }
+
+  /*let len = arr.length;
   let rows = parseInt(len /4, 10);
   let index = 0;
   let init = 0;
@@ -82,7 +170,7 @@ let addElement = (e)=>
 
     init += 4;
     main.appendChild(divs[i]);
-  }
+  }*/
 
 
 
